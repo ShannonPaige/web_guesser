1a57ebe976c31c15a8d42febaf82130c6cb6cc3f
ShannonPaige <spaige@gmail.com>
2015-10-05 16:52:02 -0600

    I5: added second extension - guessing limit

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,41 +1,47 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(101)
-# @@guesses_remaining = 5
+@@secret_number = rand(101)
+@@guesses_remaining = 5
 
 def check_guess(guess)
   if guess.nil?
-    return ""
-  elsif guess.to_i > SECRET_NUMBER + 5
-    return ["Waaaaay too high", "#FF0000"]
-  elsif guess.to_i < SECRET_NUMBER - 5
-    return ["Waaaaaay too low", "#CC3300"]
-  elsif guess.to_i > SECRET_NUMBER
-    return ["Too high", "#FE8387"]
-  elsif guess.to_i < SECRET_NUMBER
-    return ["Too low", "#DB704D"]
+    return ["", "#FFFFFF"]
   else
-    ["You guessed the secret number!!! It WAS #{SECRET_NUMBER}", "#85E685"]
+    turns = guess_decrement
+    if guess.to_i > @@secret_number + 5
+      return turns.nil? ? ["Waaaaay too high", "#FF0000"] : turns
+    elsif guess.to_i < @@secret_number - 5
+      return turns.nil? ? ["Waaaaaay too low", "#CC3300"] : turns
+    elsif guess.to_i > @@secret_number
+      return turns.nil? ? ["Too high", "#FE8387"] : turns
+    elsif guess.to_i < @@secret_number
+      return turns.nil? ? ["Too low", "#DB704D"]: turns
+    else
+      correct_answer = @@secret_number
+      reset_secret_number
+      return ["You guessed the secret number!!! It WAS #{correct_answer}.\nThe game has now been reset. Play again!", "#85E685"]
+
+    end
   end
 end
 
-# def reset_number
-#   @@guesses_remaining = 5
-#   SECRET_NUMBER = rand(101)
-# end
-#
-# def bad_guess
-#   @@guesses_remaining -= 1
-#   if @@guesses_remaining.zero?
-#     reset_number
-#     return ["You used up all your guesses!! The game will be reset.", "FF6600"]
-#   end
-# end
+def guess_decrement
+  @@guesses_remaining -= 1
+  if @@guesses_remaining == 0
+    reset_secret_number
+    return ["Time's Up! The game has been reset", "#8A2EE6"]
+  end
+end
+
+def reset_secret_number
+  @@guesses_remaining = 5
+  @@secret_number = rand(101)
+end
 
 get '/' do
   guess = params["guess"]
   message, background = check_guess(guess)
-  x = erb :index, :locals => {:guess => guess, :message => message, :background => background}
+  x = erb :index, :locals => {:message => message, :background => background, :number => @@secret_number, :guesses => @@guesses_remaining}
 
 end
